f23f91f396c8d303bc2af57b635ab932ab4ab098
Aldo Ridhoni <aldoridhoni@gmail.com>
2015-11-22 14:19:40 +0700

    rename class

---
+++ VulnDetection.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# VulnDetection.py
+"""
+Banner reader.
+"""
+import  re
+
+class VulnDetection(object):
+	def __init__(self):
+		self.db = None
+		self.result = None
+
+	def os_detect(self):
+		try:
+			os = self.result['osclass']
+			print 'OS detection accuracy ' + os['accuracy'] + '% '
+			print 'Vendor : ' + os['vendor'] + ', ' + os['osfamily'] + ' ' + os['osgen']
+		except:
+			print 'For OS detection pvascan need root privillage'
+
+	def port_info(self):
+		found_ports = self.result['tcp']
+		print 'Discovered host ports : %s' % len(found_ports)
+		for number, port in found_ports.iteritems():
+			banner = port['product'] + ' ' + port['version']
+			if (port['state'] == 'open'):
+				print '[+] PORT %s [%s] %s' % (number, port['name'], banner)
+				if len(banner) > 3:
+					self.loop_db(banner)
+			else:
+				print '[-] PORT %s [STATE:%s]' % (number, port['state'])
+
+	def loop_db(self, banner):
+		found = {}
+		for row in self.db:
+			if self.vulner_search(banner, row['description']):
+				found.update({row['id'] : row['description']})
+
+		if found:
+			print '| VULNERABILITY DETECTED!'
+			print '|- Description : '
+			for num, id in enumerate(found):
+				print '|    %d. %s' % (num + 1, found[id])
+				print '|    | For more information please visit url below'
+				print '|    |_ https://www.exploit-db.com/exploits/' + id +'/'
+			print '|-', len(found), 'exploits found,'
+			print '|__ Please contact the aplication\'s vendor to patch the vulnerability\n'
+
+	def vulner_search(self, banner, string):
+		s = re.compile(banner, flags=re.IGNORECASE)
+		if s.match(string):
+			return True
+		else:
+			return False
