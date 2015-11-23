be61cb6e241aafd139184855ab4539b058ea55bf
Aldo Ridhoni <aldoridhoni@gmail.com>
2015-11-23 16:56:56 +0700

    python3

--- VulnDetection.py
+++ VulnDetection.py
@@ -4,6 +4,7 @@
 """
 Banner reader.
 """
+from __future__ import print_function
 import  re
 
 class VulnDetection(object):
@@ -14,22 +15,22 @@ class VulnDetection(object):
 	def os_detect(self):
 		try:
 			os = self.result['osclass']
-			print 'OS detection accuracy ' + os['accuracy'] + '% '
-			print 'Vendor : ' + os['vendor'] + ', ' + os['osfamily'] + ' ' + os['osgen']
-		except:
-			print 'For OS detection pvascan need root privillage'
+			print("OS detection accuracy %s%" % os['accuracy'])
+			print("Vendor :", os['vendor'], os['osfamily'], os['osgen'])
+		except KeyError:
+			print("For OS detection pvascan need root privillage")
 
 	def port_info(self):
 		found_ports = self.result['tcp']
-		print 'Discovered host ports : %s' % len(found_ports)
-		for number, port in found_ports.iteritems():
+		print("Discovered host ports : %s" % len(found_ports))
+		for (number, port) in found_ports.items():
 			banner = port['product'] + ' ' + port['version']
 			if (port['state'] == 'open'):
-				print '[+] PORT %s [%s] %s' % (number, port['name'], banner)
+				print("[+] PORT %s [%s] %s" % (number, port['name'], banner))
 				if len(banner) > 3:
 					self.loop_db(banner)
 			else:
-				print '[-] PORT %s [STATE:%s]' % (number, port['state'])
+				print("[-] PORT %s [STATE:%s]" % (number, port['state']))
 
 	def loop_db(self, banner):
 		found = {}
@@ -38,14 +39,14 @@ class VulnDetection(object):
 				found.update({row['id'] : row['description']})
 
 		if found:
-			print '| VULNERABILITY DETECTED!'
-			print '|- Description : '
+			print ("| VULNERABILITY DETECTED!")
+			print ("|- Description : ")
 			for num, id in enumerate(found):
-				print '|    %d. %s' % (num + 1, found[id])
-				print '|    | For more information please visit url below'
-				print '|    |_ https://www.exploit-db.com/exploits/' + id +'/'
-			print '|-', len(found), 'exploits found,'
-			print '|__ Please contact the aplication\'s vendor to patch the vulnerability\n'
+				print("|    %d. %s" % (num + 1, found[id]))
+				print("|    | For more information please visit url below")
+				print("|    |_ https://www.exploit-db.com/exploits/%s/" % id)
+			print("|-", len(found), "exploits found,")
+			print("|__ Please contact the aplication's vendor to patch the vulnerability\n")
 
 	def vulner_search(self, banner, string):
 		s = re.compile(banner, flags=re.IGNORECASE)
--- pvascan.py
+++ pvascan.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # pvascan.py
+# encoding = utf8
 
 """
 Copyright (c) 2015, M Habib - STMIK Akakom, Yogyakarta
 All rights reserved.
 License : BSD-3-Clause
 """
+from __future__ import print_function
+from __future__ import absolute_import
+
+import argparse
+import csv
+import os
+import platform
+import datetime
 
 try:
-	import argparse, wget, csv, os, platform, datetime
 	from ConfigParser import ConfigParser
-	from nmap import PortScanner
-	from VulnDetection import VulnDetection
 except ImportError:
-	print '[-] Python error, some library cannot imported.'
-	print '| pvascan importing additional library : '
-	print '|	python-nmap, wget'
-	print '|__ Try: pip install <python-library>\n'
+	from configparser import ConfigParser
+try:
+	import wget
+	from nmap import PortScanner
+except ImportError as e:
+	print("[-]", e)
+	print("| pvascan importing additional library :")
+	print("|	python-nmap, wget")
+	print("|__ Try: pip install <python-library>\n")
 	exit(0)
+
+from io import open
+from VulnDetection import VulnDetection
 
 cnfile	= 'config.ini'
 dbfile	= 'files.csv'
@@ -32,11 +46,11 @@ def create_config():
 	section.
 	"""
 	if not os.path.isfile(cnfile):
-		with open(cnfile, 'wb') as configfile:
+		with open(cnfile, 'wb', encoding='utf-8') as configfile:
 			config.add_section('Configuration')
 			config.set('Configuration', 'database', dbfile)
 			config.write(configfile)
-			print '[+] New configuration created with default value on file %s.\n' % cnfile
+			print("[+] New configuration created with default value on file %s.\n" % cnfile)
 
 def load_config():
 	global dbfile
@@ -44,47 +58,46 @@ def load_config():
 		config.read(cnfile)
 		dbfile = config.get('Configuration', 'database')
 	except:
-		print '[-] Something is wrong while reading configuration file,'
+		print("[-] Something is wrong while reading configuration file.")
 		exit(0)
 
 def update_config(db):
-	"""
-	Updating config with db variable
-	"""
+	"""Updating config with db variable."""
 	try:
 		config.read(cnfile)
 		config.set('Configuration', 'database', db)
-		with open(cnfile, 'wb') as conf:
+		with open(cnfile, 'wb', encoding='utf-8') as conf:
 			config.write(conf)
-			print '[+] Configuration updated on file %s.\n' % cnfile
+			print("[+] Configuration updated on file %s.\n" % cnfile)
 	except:
-		print '[-] Error while updating configuration file!\n'
+		print("[-] Error while updating configuration file!\n")
 
 def get_db():
 	try:
 		# Exploit-DB files.csv
 		db = wget.download('https://raw.githubusercontent.com/offensive-'
 		'security/exploit-database/master/files.csv')
-		print ''
+		print()
 	except:
-		print '[-] Error while downloading file database!'
+		print("[-] Error while downloading database file!")
 
 def validate_db(dbfile):
 	if not os.path.isfile(dbfile):
-		print '[-] Database is not exist'
+		print("[-] Database is not exist")
 		exit(0)
 
 def load_db(dbfile):
 	"""
-	Reload config and load dbfile
-	Return db object
+	Reload config and load dbfile.
+	Return db object.
 	"""
 	try:
-		db = csv.DictReader(open(dbfile))
+		f = open(dbfile, 'r', encoding='utf-8')
+		db = csv.DictReader(f)
 		return db
 	except:
-		print '[-] Vulnerability database is not selected.'
-		print '|__ Please try \'./pvascan.py -h\'\n'
+		print("[-] Vulnerability database is not loading.")
+		print("|__ Please try ./pvascan.py -h\n")
 		exit(0)
 
 def opt_menu():
@@ -114,17 +127,18 @@ def opt_menu():
 		ports = options.port
 
 def nm_scan(hosts, ports, args='-T4 -A'):
-	print 'From %s %s' % (platform.uname()[0], platform.uname()[2])
-	print 'On ' + datetime.datetime.now().ctime()
-	print 'Scanning for host ' + hosts
+	print("From" , platform.uname()[0], platform.uname()[2])
+	print("On",  datetime.datetime.now().ctime())
+	print("Scanning for host", hosts)
 	try:
 		nm = PortScanner()
 		result = nm.scan(hosts=hosts, ports=ports, arguments=args, sudo=False)
 		return result
 	except:
-		print '[-] Error!!! Something is wrong,'
-		print '| (network trouble / nmap problem)'
-		print '|__ Please try \'./pvascan.py -h\'\n'
+		print("[-] Error!!! Something is wrong,")
+		print("| (network trouble / nmap problem) ")
+		print("| make sure you have nmap installed ")
+		print("|__ Please try ./pvascan.py -h\n")
 		exit(0)
 
 def main():
@@ -134,10 +148,11 @@ def main():
 	validate_db(dbfile)
 
 	nmap_result = nm_scan(hosts, ports)
+	# nmap_result = {'nmap': {'scanstats': {'uphosts': '2', 'timestr': 'Sun Nov 22 23:45:21 2015', 'downhosts': '0', 'totalhosts': '2', 'elapsed': '22.63'}, 'scaninfo': {'tcp': {'services': '22,80', 'method': 'connect'}}, 'command_line': 'nmap -oX - -p 80, 22 -T4 -A 127.0.0.1-2'}, 'scan': {'127.0.0.2': {'status': {'state': 'up', 'reason': 'syn-ack'}, 'hostnames': [], 'vendor': {}, 'addresses': {'ipv4': '127.0.0.2'}, 'tcp': {80: {'product': 'nginx', 'state': 'open', 'version': '1.8.0', 'name': 'http', 'conf': '10', 'script': {'html-title': 'Welcome to nginx!'}, 'extrainfo': '', 'reason': 'syn-ack', 'cpe': ''}, 22: {'product': 'OpenSSH', 'state': 'open', 'version': '5.9p1 Debian 5ubuntu1.7', 'name': 'ssh', 'conf': '10', 'extrainfo': 'protocol 2.0', 'reason': 'syn-ack', 'cpe': ''}}}, '127.0.0.1': {'status': {'state': 'up', 'reason': 'syn-ack'}, 'hostnames': [{'type': 'PTR', 'name': 'localhost'}], 'vendor': {}, 'addresses': {'ipv4': '127.0.0.1'}, 'tcp': {80: {'product': 'nginx', 'state': 'open', 'version': '1.8.0', 'name': 'http', 'conf': '10', 'script': {'html-title': 'Welcome to nginx!'}, 'extrainfo': '', 'reason': 'syn-ack', 'cpe': ''}, 22: {'product': 'OpenSSH', 'state': 'open', 'version': '5.9p1 Debian 5ubuntu1.7', 'name': 'ssh', 'conf': '10', 'script': {'ssh-hostkey': '1024 fe:c6:fe:4f:02:1e:a1:86:23:b0:ea:e4:f0:bb:53:8d (DSA)\n2048 da:4c:cf:2d:84:fa:2c:15:1f:68:74:f6:c6:4a:63:f2 (RSA)'}, 'extrainfo': 'protocol 2.0', 'reason': 'syn-ack', 'cpe': ''}}}}}
 	pva = VulnDetection()
-	for host, result in nmap_result['scan'].iteritems():
-		print "=============="
-		print 'IP : %s' % host
+	for (host, result) in nmap_result['scan'].items():
+		print("==============")
+		print("IP : %s" % host)
 		pva.db = load_db(dbfile)
 		pva.result = result
 		pva.os_detect()
